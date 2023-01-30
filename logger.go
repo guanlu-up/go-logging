174786cd528534457404e1e2d70c7996ca95a78f
guanlu-up <guanlu.up@gmail.com>
2023-01-30 11:07:32 +0800

    check file size

--- logger.go
+++ logger.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path"
 	"runtime"
+	"strings"
 	"time"
 )
 
@@ -37,6 +38,35 @@ var LevelMapper = map[LogLevel]string{
 	FATAL:   "FATAL",
 }
 
+// canBeWrite 判断当前日志文件是否可以被写入
+// 当日志文件的容量大于Logger对象指定的最大容量值时就需要重新打开一个新文件
+func canBeWrite(logger *Logger) bool {
+	info, _ := logger.fio.Stat()
+	if uint64(info.Size()) < logger.maxSize {
+		return true
+	}
+	_ = logger.CloseFile()
+
+	// 重命名原文件
+	dir, filename := path.Split(logger.filepath)
+	date := time.Now().Format("20060102150405")
+	index := strings.LastIndex(filename, ".")
+	backupFile := fmt.Sprintf("%s_%s%s", filename[:index], date, filename[index:])
+	err := os.Rename(logger.filepath, path.Join(dir, backupFile))
+	if err != nil {
+		return false
+	}
+
+	// 重新创建原文件
+	mode := os.O_WRONLY | os.O_CREATE | os.O_APPEND
+	fio, err := os.OpenFile(logger.filepath, mode, logger.perm)
+	if err != nil {
+		return false
+	}
+	logger.fio = fio
+	return true
+}
+
 func output(logger *Logger, level LogLevel, message string) {
 	// 如果logger对象的输出等级大于传入的level则忽略这条日志
 	if logger.Level > level {
@@ -57,8 +87,11 @@ func output(logger *Logger, level LogLevel, message string) {
 
 	// 如果已经指定并打开了日志文件,则将日志一同记录到文件中
 	if logger.fio != nil {
-		_, err := logger.fio.WriteString(msg)
+		if !canBeWrite(logger) {
+			return
+		}
 
+		_, err := logger.fio.WriteString(msg)
 		if err != nil {
 			fmt.Println(err)
 			err = logger.CloseFile()
@@ -83,7 +116,7 @@ func (log *Logger) Error(message string) {
 }
 
 // NewLogger 创建一个新的Logger对象
-// param level: 表示日志输出的等级, 低于此等级的日志则不被输出
+// 	param level: 表示日志输出的等级, 低于此等级的日志则不被输出
 // 日志等级包括: TRACE DEBUG INFO WARNING ERROR FATAL
 func NewLogger(level LogLevel) *Logger {
 	if level < TRACE || level > FATAL {
@@ -94,10 +127,13 @@ func NewLogger(level LogLevel) *Logger {
 }
 
 // SetFile 设置记录日志的文件
-// param filepath: 文件路径,需要为完整路径
-// param perm: 文件自身的权限,如 0644
-// param maxSize: 文件最大可存储的容量
+// 	param filepath: 文件路径,需要为完整路径
+// 	param perm: 文件自身的权限,如 0644
+// 	param maxSize: 文件最大可存储的容量,单位Bit
 func (log *Logger) SetFile(filepath string, perm os.FileMode, maxSize uint64) {
+	if maxSize <= 0 {
+		panic("maxSize value invalid!, required > 0")
+	}
 
 	log.filepath = filepath
 	log.perm = perm
